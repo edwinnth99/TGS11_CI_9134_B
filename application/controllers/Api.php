ff408c88d87f9758712e3a598fb520a6a5cc10cf
edwin <edwinnathaniel14@gmail.com>
2019-11-20 20:01:58 +0700

    my first commit

---
+++ application/controllers/Api.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . 'third_party/REST_Controller.php';
+require APPPATH . 'third_party/Format.php';
+use Restserver\Libraries\REST_Controller;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+class Api extends REST_Controller {
+    public function __construct() {
+        parent::__construct();
+        
+        // Load these helper to create JWT tokens
+        $this->load->helper(['jwt', 'authorization']);    
+    }
+    public function hello_get()
+    {
+        $tokenData = 'Hello World!';
+        
+        // Create a token
+        $token = AUTHORIZATION::generateToken($tokenData);
+        // Set HTTP status code
+        $status = parent::HTTP_OK;
+        // Prepare the response
+        $response = ['status' => $status, 'token' => $token];
+        // REST_Controller provide this method to send responses
+        $this->response($response, $status);
+    }
+}
